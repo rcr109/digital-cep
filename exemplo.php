29976069add00f91c0b5f65429213b49b36c68d8
rcr109 <rcr109@gmail.com>
2019-12-27 23:31:56 -0300

    Versão inicial do buscador de CEP

---
+++ exemplo.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use rcr\DigitalCep\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipcode('01001000');
+
+print_r($resultado);
+
+?>
